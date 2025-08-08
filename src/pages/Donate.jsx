1c0bfde696c5fca682b14db25b219d0b30b39015
anlongawf <88261950+anlongawf@users.noreply.github.com>
2025-08-08 11:04:39 +0700

    Edit: images bank, product

--- src/pages/Donate.jsx
+++ src/pages/Donate.jsx
@@ -25,7 +25,7 @@ export default function Donate() {
         </p>
         <div className="donate-qr-box">
           <img
-            src="https://cdn.discordapp.com/attachments/1261785923239018589/1400641030419779614/IMG_6606.jpg?ex=688d601c&is=688c0e9c&hm=365afd1e0e8bb453c9dff88789c2a646136cd3b59f98304af55c3d476fec97e4&"
+            src="/images/qr_bank.jpg"
             alt="Donate QR Code"
             className="donate-qr"
           />
